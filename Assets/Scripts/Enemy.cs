3d5be909d4f045e240ed94d2b9c8f75899fe1e07
Caxatii <caxatiiarmy@gmail.com>
2025-02-12 16:17:06 +0300

    Root

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,6 +2,13 @@
 
 public class Enemy : MonoBehaviour
 {
+    private void Awake()
+    {
+        float destroyTime = 10;
+
+        Destroy(gameObject, destroyTime);
+    }
+
     private void FixedUpdate()
     {
         transform.Translate(transform.up * Time.fixedDeltaTime, Space.World);
